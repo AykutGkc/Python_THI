83005197556f71adb4061b937cf2f546580e4d1a
Aykut Gökce <aykutgokce2016@gmail.com>
2023-10-25 18:33:35 +0200

    25.10.23-THI

---
+++ for.py
@@ -0,0 +1,24 @@
+""" for i in range(0, 10):
+    if(i%2==0):
+        print(i)
+    else:
+        print(i, "ist ungerade")
+
+for i in range(0,10,2):
+        print(i)
+"""
+
+customer=["Jimmy","Kim","Stacie","Hassan","Else","Elif"]
+
+for i in customer:
+    print(i)
+
+x=0
+
+while (x<=20):
+    print(x)
+
+    x=x+1
+
+    if(x>=10):
+        break
---
+++ kugel.py
@@ -0,0 +1,12 @@
+import math
+r=float(input("Radius: "))
+pi=math.pi;
+
+#Oberflache berechnen
+O=4 * pi * r**2
+print("Oberflache: ",O)
+
+#Volumen berechnen
+
+V=4/3 * pi * r**3
+print("Volumen: ", V)
